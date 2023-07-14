8ab0048d3433d7d9be014bcfe7ff43824677d377
andres-chirinos <andreschirinos590@gmail.com>
2023-07-14 18:10:22 -0400

    Squashed commit of the following:
    commit a3060a1950df6c48cabdfacc72cb9f0bd3336d48
    Author: andres-chirinos <andreschirinos590@gmail.com>
    Date:   Fri Jul 14 17:51:22 2023 -0400
    
        Newdatabasesystem
    
    commit cadeb88bd99697f9102a6f11400814f17082377c
    Author: andres-chirinos <andreschirinos590@gmail.com>
    Date:   Thu Jun 29 17:02:06 2023 -0400
    
        Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -13,8 +13,9 @@ from quart import (
 from quart_discord import *
 from logging.config import dictConfig
 from dotenv import load_dotenv
-from memory import *
-import os, redis
+from lib.role_connection import RoleConnection
+import os, redis, asyncio, pymongo
+from datetime import datetime
 
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 load_dotenv(dotenv_path=".env")
@@ -31,29 +32,35 @@ Cache = redis.from_url(
 # Aplicación web
 url = os.getenv("RAILWAY_STATIC_URL", "localhost")
 
-
 app = Quart(__name__, root_path="src")
 app.secret_key = os.environ.get("DISCORD_CLIENT_SECRET")
+
 app.config["DISCORD_CLIENT_ID"] = os.getenv("DISCORD_CLIENT_ID")
 app.config["DISCORD_BOT_TOKEN"] = os.getenv("DISCORD_BOT_TOKEN")
+
 app.config["DISCORD_CLIENT_SECRET"] = os.getenv("DISCORD_CLIENT_SECRET")
 app.config["DISCORD_REDIRECT_URI"] = "https://" + url + "/oauth/callback"
 
+app.config["METADATA_SET"] = Cache.get("registermetadata")
+
+Memoria = pymongo.MongoClient(os.getenv("MONGO_URL"))
 
 discord_session = DiscordOAuth2Session(app)
+role_connection = RoleConnection(app, Memoria)
 
+# asyncio.run(roles.set_role_connection(list(Cache.get("registermetadata"))))
 
 @app.route("/")
 async def index():
     try:
-        user = await discord_session.fetch_user()
+        user = await discord_session.fetch_user() or None
     except:
         user = None
         pass
     return await render_template("index.html", user=user)
 
 
-@app.route("/oauth/", defaults={"redirect": "/", "prompt": "false"})
+@app.route("/oauth/", defaults={"redirect": "", "prompt": "false"})
 @app.route("/oauth/<path:redirect>/<string:prompt>")
 async def oauth(redirect, prompt):
     try:
@@ -76,6 +83,9 @@ async def oauth(redirect, prompt):
 @app.route("/oauth/callback")
 async def callback():
     data = await discord_session.callback()
+    tokens = await discord_session.get_authorization_token()
+    user = await discord_session.fetch_user()
+    Memoria.get_database('master').get_collection('users').update_one({'_id':user.id}, {"$set": {"bot_tokens": tokens}})
     return redirect(url_for(".index") + data["redirect"])
 
 
@@ -90,7 +100,10 @@ async def close():
 async def verify():
     tokens = await discord_session.get_authorization_token()
     user = await discord_session.fetch_user()
-    await update_role_connection(tokens, user.id)
+    await role_connection.push_role_connection(
+        bot_access_token=tokens["access_token"],
+        body=await role_connection.get_role_data(user.id),
+    )
     return await render_template("verify.html", user=user)
 
 
@@ -98,22 +111,8 @@ async def verify():
 @requires_authorization
 async def profile():
     user = await discord_session.fetch_user()
-
-    if request.method == "POST":
-        form = await request.form
-        if len(form["username"]) >= 3:
-            await update_one(
-                "master",
-                str(user.id),
-                {"type": "identification"},
-                {"$set": {"platform_username": form["username"], "verified": False}},
-            )
-
-    data = await find_one("master", str(user.id), {"type": "identification"})
-    if not data:
-        data = await create_identification(user.id)
-
-    return await render_template("user.html", user=user, data=data)
+    data = role_connection.get_role_data(user.id)
+    return await render_template("user.html", user=user.to_json(), data=data)
 
 
 @app.errorhandler(Unauthorized)
@@ -141,6 +140,12 @@ async def http_exception(e):
 
 @app.errorhandler(Exception)
 async def exception(e):
+   discord_session.revoke()
+   return await make_response(jsonify({"message": str(e)}))
+
+
+@app.errorhandler(Exception)
+async def exception(e):
     discord_session.revoke()
     return await make_response(jsonify({"message": str(e)}))
 
@@ -156,7 +161,4 @@ if __name__ == "__main__":
             },
         }
     )
-    app.run(
-        host=os.environ.get("HOST"),
-        port=os.environ.get("PORT"),
-    )
+    app.run(host=os.environ.get("HOST"), port=os.environ.get("PORT"), debug=True)
